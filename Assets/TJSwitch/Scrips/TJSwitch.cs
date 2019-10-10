6c65d45ba952e9cdcfa84c3637c4ce643f45efe9
TaeJun <zmddl741@naver.com>
2019-10-10 18:18:38 +0900

    색상변경
    색상변경

--- Assets/TJSwitch/Scrips/TJSwitch.cs
+++ Assets/TJSwitch/Scrips/TJSwitch.cs
@@ -8,27 +8,43 @@ public class TJSwitch : MonoBehaviour
     [Range(0, 3)]
     public float moveDuration = 3f;                       // TODO: 스위치 이동 애니메이션 시간
 
+    //Color
+    public Color handleColor = Color.white;
+    public Color offBackgroundColor =Color.green;
+    public Color onBackgroundColor= Color.blue;
+
     const float totalHandleMoveLength = 72f;
     const float halfMoveLenght = totalHandleMoveLength / 2;
 
     Image handleImage;                                   // TODO: 스위치 핸들 이미지
     Image backgroundImage;                               // TODO: 스위치 배경 이미지
     RectTransform handleRectTransform;                   // TODO: 스위치 핸들RectTransform
+
+    // Coroutine
+    Coroutine moveHandleCoroutine;
+    Coroutine changeBackgroundColorCoroutine;            // 배경색 변경 코루틴
     void Start()
     {
         GameObject handleObject = transform.Find("Handle").gameObject;
 
         handleRectTransform = handleObject.GetComponent<RectTransform>();
 
-        isOn = true;
+        //Handle Image
+        handleImage = handleObject.GetComponent<Image>();
+        handleImage.color = handleColor;
+
+        //Background Image
+        backgroundImage = GetComponent<Image>();
+        backgroundImage.color = offBackgroundColor;
 
         if (isOn)
-        
+
             handleRectTransform.anchoredPosition = new Vector2(halfMoveLenght, 0);
-            else
-                handleRectTransform.anchoredPosition = new Vector2(-halfMoveLenght, 0);
-        
-        void OnClickSwitch()
+        else
+            handleRectTransform.anchoredPosition = new Vector2(-halfMoveLenght, 0);
+    }
+    
+       public void OnClickSwitch()
         {
             isOn = !isOn;
 
@@ -38,16 +54,40 @@ public class TJSwitch : MonoBehaviour
 
             float ratio = Mathf.Abs(distance.x) / totalHandleMoveLength;
             float duration = moveDuration * ratio;
-           
-            StartCoroutine(moveHandle(fromPosition,toPosition, duration));
-            
+
+        if (moveHandleCoroutine != null)
+        {
+            StopCoroutine(moveHandleCoroutine);
+            moveHandleCoroutine = null;
         }
+           
+            MoveCoroutine(moveHandle(fromPosition,toPosition, duration));
+        //백그라운드 컬러 체인지 코루틴
+        if(changeBackgroundColorCoroutine !=null)
+        {
+            StopCoroutine(changeBackgroundColorCoroutine);
+            changeBackgroundColorCoroutine = null;
+        }
+        changeBackgroundColorCoroutine
+            = StartCoroutine(changeBackgroundColor(fromColor, toColor, duration));
+        
+
+
+        }
+    /// <summary>
+    /// 스위치 배경색 변경 함수
+    /// </summary>
+    /// <param name="fromPosition"></param>
+    /// <param name="toPosition"></param>
+    /// <param name="duration"></param>
+    /// <returns></returns>
        IEnumerator moveHandle(Vector2 fromPosition, Vector2 toPosition, float duration)
         {
             float currentTime = 0f;
           while(currentTime < duration)
             {
-                Vector2 newPosition = Vector2.Lerp(fromPosition, toPosition, duration);
+                float t = currentTime / duration;
+                Vector2 newPosition = Vector2.Lerp(fromPosition, toPosition, t);
                 handleRectTransform.anchoredPosition = newPosition;
 
                 currentTime += Time.deltaTime;
@@ -55,8 +95,25 @@ public class TJSwitch : MonoBehaviour
             }
 
         }
-        ///1.터치 시 핸들의 위치를 바꿔주는 동작(함수)
+    ///1.터치 시 핸들의 위치를 바꿔주는 동작(함수)
 
-        //2.터치 시 스위치의 배경 색상을 바꿔주는 동장(함수)
+    //2.터치 시 스위치의 배경 색상을 바꿔주는 동장(함수)
+    
+    IEnumerator changeBackgroundColor(Color fromColor, Color toColor, float duration)
+    {
+        //TODO: 
+        //1.정해진 시간동안 색상을 변경시켜야 한다.
+        float currentTime = 0f;
+        while (currentTime < duration)
+        {
+            float t = currentTime / duration;
+            Color newColor = Color.Lerp(fromColor, toColor, t);
+
+            backgroundImage.color = newColor;
+
+            currentTime += Time.deltaTime;
+            yield return null;
+        }
+
     }
-}
+    }
